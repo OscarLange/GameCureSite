7ae731aba9d20342755ef84b7930af0650198c2b
OScar Lange <langeoscar96@googlemail.com>
2021-01-26 11:23:01 +0100

    Added automatic Email

--- routes/addEmail.js
+++ routes/addEmail.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer'); 
 
 /* GET users listing. */
 router.post('/:email', function(req, res, next) {
@@ -25,6 +26,31 @@ router.post('/:email', function(req, res, next) {
             });
         }
     });
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORT
+        }
+      });
+      
+
+    var mailOptions = {
+        from: 'gamecure.tc@gmail.com',
+        to: email,
+        subject: 'Thanks for signing up to CauseCure',
+        html: '<h1>Welcome to CauseCure <br></h1><p>Thanks for joing us!<br> Our Team will register you,<br>and we will follow up with you shortly!<br><br> Kind Regards<br> Team GameCure!</p>'
+      };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      }); 
+      
 
   res.send('respond with a resource');
 });
